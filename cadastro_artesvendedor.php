7059b8a225359a638019463beb081c2133f3a29e
zzjunior <santos.junior.706@ufrn.edu.br>
2024-07-15 18:20:51 -0300

    prótotipo iniciado - cadastro artes

--- cadastro_artesvendedor.php
+++ cadastro_artesvendedor.php
@@ -4,11 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pacote de Artes do Vendedor</title>
-    <!--bootstrapp junior - adicinei aqui pra ficar com o estilo que fiz nos testes--->
+    <!--bootstrapp-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
+    <header class="bg-dark">
+        <section class="row">
+        <h1 class="col-sm-5 text-center sm-3 mt-5 text-white">
+        Personalize suas Redes Sociais e Multiplique suas vendas de Motos e Consórcios
+        </h1>
+        <p class="col-sm-6 text-center sm-3">
+        <img class="img-fluid w-50" src="https://alcancevendasonline.com.br/wp-content/uploads/2023/12/pacote4-1.png" alt="alcance vendas online">
+        </p>
+        </section>
+    </header>
+    
+
     
 </body>
 </html>
